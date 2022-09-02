160fc939c6565fbd479a4e67760279635773ff0a
DRlamichhane1 <drlamichhane1@uclan.ac.uk>
2022-09-02 12:10:36 +0100

    ScanItem negative test. no exception means pass

--- XUnitTestProject/UnitTest1.cs
+++ XUnitTestProject/UnitTest1.cs
@@ -6,6 +6,9 @@ namespace XUnitTestProject
 {
     public class UnitTest1
     {
+        private readonly Item itemA99 = new("A99", 0.5m);
+
+
         [Fact]
         public void Test1()
         {
@@ -15,9 +18,20 @@ namespace XUnitTestProject
         [Fact]
         public void createitem()
         {
-            Item newItem = new Item("A99", 0.5m);
+            Item newItem = itemA99;
 
-            Assert.Equal("A99", newItem.SKU);
+            Assert.Equal("A99", newItem.SKU);//testing object creation
         }
+
+        [Fact]
+        public void ScanItem()
+        {
+            var checkout = new Checkout();
+            var item = itemA99;
+
+            Assert.Throws<Exception>(
+                () => checkout.Scan(item));//negative test for scan item
+        }
+
     }
 }
