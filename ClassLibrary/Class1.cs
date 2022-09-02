23e12b68c1b4556ffcf4c39f838a758552d34506
DRlamichhane1 <drlamichhane1@uclan.ac.uk>
2022-09-02 11:53:44 +0100

    Class Library created

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace ClassLibrary
+{
+        public class Checkout
+        {
+            public decimal Total()
+            {
+                return 0m;
+            }
+
+            public void Scan(Item item)
+            {
+
+            }
+        }
+
+        public class Item
+        {
+     
+    
+
+}
